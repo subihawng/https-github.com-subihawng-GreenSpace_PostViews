8cea44045ffe6dc71517d80caa99585309d3efad
subihwang <ghkdtnql@gmail.com>
2022-11-23 21:01:48 +0900

    Initial Commit

---
+++ Post/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Post
+//
+//  Created by 황수비 on 2022/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
